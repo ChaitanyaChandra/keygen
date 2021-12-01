ab87188b4743463978c22c805346f8ff8fd99db0
Chaitanya <56665907+ChaitanyaChandra@users.noreply.github.com>
2021-12-01 23:48:55 +0530

    Update code.sh

--- code.sh
+++ code.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-HOSTNAME=`server` ssh-keygen  -t  rsa -C "$HOSTNAME" -f "$HOME/.ssh/id_rsa" -P ""
+
+ssh-keygen  -t  rsa -C "$HOSTNAME" -f "$HOME/.ssh/id_rsa" -P ""
+
+Host xxx
+    HostName 10.XXX
+    User XXX
+    Port XXX
+    IdentityFile ~/.ssh/XXX
+
+chmod 600 ~/.ssh/config
+ssh-copy-id Host
